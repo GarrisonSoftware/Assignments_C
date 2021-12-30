1edbae1278ed33416cdcb2b60c6233b6f5ef2463
GarrisonSoftware <garrisoncl21@students.ecu.edu>
2021-12-29 16:32:55 -0800

    Addition_Integers

---
+++ Assignment/Addition_Integers.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+
+using namespace std ;
+
+int main()
+{
+    int x , y ;
+    int Total ;
+
+    cout << "Please enter two integers with a space between them: " , cin >> x , cin >> y ;
+    
+    Total = x + y ;
+
+    cout << "Your total is: " << x << " + " << y << " = " << Total << endl ;
+    
+
+}
